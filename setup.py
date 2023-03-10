2d596937b503be6e7a5931e13ce0aa818b30c445
Nishant Gupta <shub002gupta@gmail.com>
2023-03-11 00:30:08 +0530

    Remove unwanted files and keep model specific

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.1"
 
-REPO_NAME = "NYRenderer"
-AUTHOR_USER_NAME = "NishKoder"
-SRC_REPO = "NYRenderer"
-AUTHOR_EMAIL = "shub002gupta@gmail.com"
+REPO_NAME = "<YOUR REPO NAME>"
+AUTHOR_USER_NAME = "<YOUR USERNAME>"
+SRC_REPO = "<YOUR REPO NAME>"
+AUTHOR_EMAIL = "<YOUR EMAIL NAME>"
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
--- template.py
+++ template.py
@@ -16,18 +16,7 @@ logging.info("Creating Project by name: %s",project_name)
 
 # list of files:
 list_of_files = [
-    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    "tests/__init__.py",
-    "tests/unit/__init__.py",
-    "tests/integration/__init__.py",
-    "init_setup.sh",
-    "requirements.txt",
-    "requirements_dev.txt",
-    "setup.py",
-    "pyproject.toml",
-    "setup.cfg",
-    "tox.ini"
 ]
 
 for file_path in list_of_files:
